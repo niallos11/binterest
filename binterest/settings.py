b693221c33b175157f5c02ea85fe3527779c9a3e
niallos11 <>
2023-09-12 23:12:13 +0000

    remove config in settings.py

--- binterest/settings.py
+++ binterest/settings.py
@@ -53,8 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ]
 
 ROOT_URLCONF = 'binterest.urls'
