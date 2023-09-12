dbf043fe7ed4f40dbfb4cb340b75dbe7b3e2bb80
niallos11 <>
2023-09-12 23:05:18 +0000

    remove  config to settings.py

--- binterest/settings.py
+++ binterest/settings.py
@@ -151,11 +151,9 @@ CURRENCY_CODE = 'EUR'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
