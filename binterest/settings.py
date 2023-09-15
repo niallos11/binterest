8a6fd08c33561619543bbdde24f8251f451dd05c
niallos11 <>
2023-09-15 15:25:33 +0000

    Remove dev config for  setting.py

--- binterest/settings.py
+++ binterest/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY', ''),
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = 'FALSE'
 ALLOWED_HOSTS = ['8000-niallos11-binterest-xay8xf3i81.us2.codeanyapp.com', 'app-binterest.herokuapp.com']
 
